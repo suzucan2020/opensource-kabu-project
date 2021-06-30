ac39bdbe2a43654c44ef1db7e3735bf7e3699b6a
suzucan2020 <60060661+suzucan2020@users.noreply.github.com>
2021-06-30 14:21:02 +0900

    Update scraping-month-kabuka-from-nikkei.py

--- scraping/scraping-month-kabuka-from-nikkei.py
+++ scraping/scraping-month-kabuka-from-nikkei.py
@@ -131,12 +131,15 @@ def send_message(msg):
         print ("Exception Error: ", e)
         sys.exit(1)
 
+
 def is_num(s):
     return s.replace(',', '').replace('.', '').replace('-', '').isnumeric()
 
 def lambda_handler(event, context):
     # TODO implement
 
+    # Lambdaの実行時間は最大15分なので、時間を計測する
+    # 処理が完了しない場合、続きから処理させるために必要な情報をreturnする
     start_time = time.time()
     
     check_value = []
@@ -146,10 +149,12 @@ def lambda_handler(event, context):
         send_message(msg)
         print(msg)
     
+    # indexからcodes数分ループ
     for num in range(index,len(codes)):
         #index = event["counter"]
         #print(index)
         #code = codes[index]
+        # 銘柄コードを格納
         code = codes[num]
         print(num,":",  code)
         BUCKET_NAME = "kabu-data"
@@ -164,8 +169,11 @@ def lambda_handler(event, context):
         # print(dt_now_year)
         # print(dt_now_month_day)
 
+        # 株価を持ってくるURL
         url_name = 'https://www.nikkei.com/nkd/company/history/dprice/?scode=' + code
 
+        # URLが開けるまでリトライする
+        # サーバーへの負荷を考慮してsleep 1とする
         retry_count = 0
         while True:
             html_data = urllib.request.urlopen(url_name)
@@ -173,42 +181,60 @@ def lambda_handler(event, context):
                 break
             retry_count += 1
             sleep(1)
+            
+        # ファイルの作成、一行目は銘柄コード、2行目はヘッダとする
         with open(LOCAL_FILE, 'w') as fd:
             # print(fd.read())
             fd.write(code)
             fd.write('\nDate,Open,High,Low,Close,Volume,Trading Value')
 
-    
+       # 以下株価抽出処理
         value_list = []
         # value_list.append(dt_now.date())
         hit_date = 0
         counter = 0
         counter2 = 0
         next_read = 0
+        # htmlを一行ずつ読み込む
         for line in html_data.readlines():
             # print(line)
             decode_line = line.decode('utf-8')
             # print(decode_line)
 
+            # 月/日のみ取り出す、曜日はいらない
+            # 抽出データ例
+            # <th class="a-taC">
+            #    6/29（火）</th>
             if next_read == 1:
                 tmp = decode_line.strip()
                 tmp2 = tmp.split('（')
                 # print(tmp2[0])
                 value_list.append(tmp2[0])
                 next_read = 0
-
+                
+            # a-taCで引っかけて日付を取り出す
+            # ※最初の7回は以下のようにヘッダとなるのですっとばす
+            #        <th class="a-taC">日付</th>
+            #        <th class="a-taC">始値</th>
+            #        <th class="a-taC">高値</th>
+            #        <th class="a-taC">安値</th>
+            #        <th class="a-taC">終値</th>
+            #        <th class="a-taC">売買高</th>
+            #        <th class="a-taC">修正後終値</th>
             if "a-taC" in decode_line:
                 # print(decode_line)
                 counter += 1
                 if counter > 7:
                     next_read = 1
 
+            # a-taR a-wordBreakAllで引っかけて日付、始値、、、修正後終値を取り出す
             if "a-taR a-wordBreakAll" in decode_line:
                 hit_date = 1
-
+            # 数値のみ抜き出す
             if hit_date == 1:
                 tmp = decode_line.split(">")[1]
                 tmp2 = tmp.split("<")[0]
+                # カンマが入ると数値として正しく処理できないで取り除く
                 tmp3 = tmp2.replace(',', '')
                 if is_num(tmp3) == True:
                     value_list.append(float(tmp3))
@@ -216,15 +242,19 @@ def lambda_handler(event, context):
                     value_list.append(tmp3)
                 counter2 += 1
 
+                # counter2 == 6　で一日分のデータとなる
                 if counter2 == 6:
                     counter2 = 0
+                    # csvとして扱うため、リストをカンマ区切りで文字列に変換する
                     value_str = ','.join(map(str, value_list))
                     # print(value_list)
                     if counter == 8: # counter == 8 の時最新の日付のデータである
                         check_value = value_str
-
+    
+                    # データがない場合value_listは'--'となる
                     if '--' in value_list:
                         print(value_list, ": -- bad data")
+                    # ファイルに追記していく
                     else:
                         with open(LOCAL_FILE, 'a') as fd:
                             # print(fd.read())
@@ -235,7 +265,9 @@ def lambda_handler(event, context):
         s3 = boto3.resource('s3') #S3オブジェクトを取得
         bucket = s3.Bucket(BUCKET_NAME)
         bucket.upload_file(LOCAL_FILE, KEY)
-
+        
+        # Lambdaの実行時間は最大15分なので、時間を計測する
+    　  # 処理が完了しない場合、続きから処理させるために必要な情報をreturnする
         time.sleep(0.2)
         spend_time = time.time() - start_time
         print(spend_time)
@@ -243,6 +275,7 @@ def lambda_handler(event, context):
         if spend_time > 780:
             break
     
+    # 最後まで処理が完了した場合lineにメッセージ送信する
     if len(codes) -1 == index:
         result = False
         msg = "正常チェック code: " + codes[index] + "\n" + check_value + "\nURL: https://www.nikkei.com/nkd/company/?scode=" + codes[index]
