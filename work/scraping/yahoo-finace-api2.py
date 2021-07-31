83973fb12f4cdce386fa3d63df04d921a005377e
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-31 08:13:37 +0000

    update

---
+++ work/scraping/yahoo-finace-api2.py
@@ -0,0 +1,47 @@
+from yahoo_finance_api2 import share
+from yahoo_finance_api2.exceptions import YahooFinanceError
+import pandas as pd
+
+# data_list = [ \
+#     ["4391.T", "LOGIZARD"], 
+#     ["4755.T", "RAKUTEN"],
+#     ["2303.T", "DAWN"]]
+
+import time
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# print(sys.path)
+import okap
+
+codes = [1301]
+# codes = okap.read_stock_code_list('stock-code-list/all.txt')
+
+start_time = time.time()
+
+for i, code in enumerate(codes):
+    company_code = str(code) + ".T"
+    my_share = share.Share(company_code)
+
+    try:
+        symbol_data = my_share.get_historical(share.PERIOD_TYPE_WEEK, 1000,
+                                        share.FREQUENCY_TYPE_DAY, 1)
+        df = pd.DataFrame(symbol_data.values(), index=symbol_data.keys()).T
+        df.timestamp = pd.to_datetime(df.timestamp, unit='ms')
+        df.index = pd.DatetimeIndex(df.timestamp, name='Date').tz_localize('UTC').tz_convert('Asia/Tokyo')
+        
+        df["Trading Value"] = df.close
+        df = df.drop('timestamp', axis='columns')
+        df = df.set_axis(['Open','High','Low','Close','Volume','Trading Value'], axis='columns')
+
+        print(company_code)
+        print(df)
+        # df.to_csv("test.csv")
+        spend_time = time.time() - start_time
+        print(spend_time)
+ 
+
+    except YahooFinanceError as e:
+        print(e.message)
+        pass
