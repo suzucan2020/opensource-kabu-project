79945348a47bc1ae7a1bda81934eb366e0bcc2cf
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-08-07 15:10:13 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
+date > ../github-action.log
 ls
 cd work
 echo "scraping stage"
-python3 scraping/yahoo-finace-api2.py
+python3 scraping/yahoo-finace-api2.py >> ../github-action.log
 
 echo "chaart stage"
 python3 chart/BB.py > ../docs/chart/BB.html
