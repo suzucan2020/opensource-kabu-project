6e53a66bd97c8e6a61a99099180079d43565ba56
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-08-07 15:02:15 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 ls
 cd work
+echo "scraping stage"
 python3 scraping/yahoo-finace-api2.py
+
+echo "chaart stage"
 python3 chart/BB.py > ../docs/chart/BB.html
 python3 chart/MACD.py > ../docs/chart/MACD.html
 python3 chart/IchimokuCloud.py > ../docs/chart/IchimokuCloud.html
-git status
 
 echo "Hello $1"
 time=$(date)
