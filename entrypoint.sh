e6c854b5054184d9dc2999588935894219d877a8
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-08-09 16:17:04 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,29 @@
 #!/bin/sh -l
 
-
 ls
 
 cd work
 
-echo "" > ../github-action.log
+LOG_DIR="../github-action-log/`date '+%Y-%m'`"
+LOG_FILE=$LOG_DIR/"`date '+%d'`.log"
+echo $LOG_FILE
+
+mkdir -p $LOG_DIR
+touch $LOG_FILE
 
 echo "write date"
-date >> ../github-action.log
+date >> $LOG_FILE
 
 echo "scraping stage"
-python3 scraping/yahoo-finace-api2.py >> ../github-action.log
+python3 scraping/yahoo-finace-api2.py >> $LOG_FILE
 
 echo "sell check stage"
-python3 filter/MACD-sell.py >> ../github-action.log
+python3 filter/MACD-sell.py >> $LOG_FILE
 
-echo "profti stage"
-python3 filter/profit.py >> ../github-action.log
+echo "profit stage"
+python3 filter/profit.py >> $LOG_FILE
 
-echo "chaart stage"
+echo "chart stage"
 python3 chart/BB.py > ../docs/chart/BB.html
 python3 chart/MACD.py > ../docs/chart/MACD.html
 python3 chart/IchimokuCloud.py > ../docs/chart/IchimokuCloud.html
