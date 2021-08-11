271da9bb8c791939362f5dbcf9da616502c9fee0
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-08-11 04:17:55 +0000

    update

--- work/calc_ci/calc_ci.py
+++ work/calc_ci/calc_ci.py
@@ -1,11 +1,11 @@
 for x in range(1):
-    print("{:9}|".format(1), end="")
+    print("|{:9}|".format(1), end="")
     for y in range(8):
         print("{:9}|".format(((y+1)*12)), end="")
 print("")
 
 for x in range(1):
-    print("{:9}|".format("----"), end="")
+    print("|{:9}|".format("----"), end="")
     for y in range(8):
         print("{:9}|".format("----"), end="")
 print("")
@@ -14,7 +14,7 @@ print("")
 profit = 1
 for x in range(10):
     profit += 0.01
-    print("{:8.2f}%|".format(profit), end="")
+    print("|{:8.2f}%|".format(profit), end="")
     for y in range(8):
         # print(profit, "**", "month =", ((y+1)*12))
         print("{:8.2f}%|".format(profit**((y+1)*12)), end="")
