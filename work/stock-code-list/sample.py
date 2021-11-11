501991e40e8a2e4d1f3b6872de678a46e83de28d
root <root@ip-172-31-45-20.ap-northeast-1.compute.internal>
2021-11-11 01:34:37 +0000

    update

--- work/stock-code-list/sample.py
+++ work/stock-code-list/sample.py
@@ -21,6 +21,6 @@ for line_b in list_b:
             print(line_a.strip())
             flag = 1
     if flag != 1:
-        o_str = "2021-11-04 22:30:00+09:00,{}".format(line_b.strip())
+        o_str = "2021-11-09 22:30:00+09:00,{}".format(line_b.strip())
         print(o_str)
         # print(line_b.strip())
